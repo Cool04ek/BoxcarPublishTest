c647c47cb35cbd61296fa1e907c5b5729fb9eb51
Roman Kozlov <vafli9@gmail.com>
2016-04-26 14:38:23 +0300

    custom url

--- java-publisher-client/src/main/java/io/boxcar/publisher/Demo.java
+++ java-publisher-client/src/main/java/io/boxcar/publisher/Demo.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,55 +16,56 @@ import io.boxcar.publisher.model.Tag;
 
 /**
  * This is the main entry point to execut the demo application.
- * 
+ * <p/>
  * This demo just connects with the Boxcar Universal Push Notification
  * Platform and publishes a "Hello World" content to all Android devices
- * using the URL signature authorization method. 
- * 
- * @author jpcarlino
+ * using the URL signature authorization method.
  *
+ * @author jpcarlino
  */
 public class Demo {
 
     static org.apache.log4j.Logger logger;
+
     static {
         logger = org.apache.log4j.Logger.getLogger(Demo.class);
     }
 
-	static final String PROP_PUBLISH_KEY = "io.boxcar.publisher.key";
-	static final String PROP_PUBLISH_SECRET = "io.boxcar.publisher.secret";
+    static final String PROP_PUBLISH_KEY = "io.boxcar.publisher.key";
+    static final String PROP_PUBLISH_SECRET = "io.boxcar.publisher.secret";
 
-	static String PUBLISH_KEY;
-	static String PUBLISH_SECRET;
+    static String PUBLISH_KEY;
+    static String PUBLISH_SECRET;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		APIClient apiClient = null;
-		
-		// 1. Build the api client
-		
-		try {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-			Properties properties = loadProperties();
+        APIClient apiClient = null;
+
+        // 1. Build the api client
+
+        try {
+
+            Properties properties = loadProperties();
 
             PUBLISH_KEY = properties.getProperty(PROP_PUBLISH_KEY);
             PUBLISH_SECRET = properties.getProperty(PROP_PUBLISH_SECRET);
 
-			apiClient = new APIClient(properties, PUBLISH_KEY, PUBLISH_SECRET,
-					RequestStrategy.URL_SIGNATURE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+            apiClient = new APIClient(properties, PUBLISH_KEY, PUBLISH_SECRET,
+                    RequestStrategy.URL_SIGNATURE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
         // 2. Publish
 
         try {
             StringBuffer text = getText(args);
-            int id = sendPush(text.toString(), "android", 120, apiClient);
+            StringBuffer url = getUrl(args);
+            int id = sendPush(text.toString(), url.toString(), 120, apiClient);
             logger.debug("Push sent with id " + id);
         } catch (Exception e) {
             e.printStackTrace();
@@ -109,15 +111,12 @@ public class Demo {
             System.exit(1);
         }
 
-	}
+    }
 
-    private static int sendPush(String text, String targetOS, int ttl, APIClient apiClient) throws IOException {
+    private static int sendPush(String text, String url, int ttl, APIClient apiClient) throws IOException {
         Alert<String> alert = new Alert<String>(text);
         // remove this line if you just want to send it to all registered
         // devices
-        alert.setTargetOS(targetOS);
-        // do not keep the push more than 2 minutes if device is not
-        // available
         alert.setTTL(ttl);
 
         List<String> tags = new ArrayList<String>();
@@ -128,6 +127,8 @@ public class Demo {
         // make this push 'normal' priority, meaning the gateway (GCM, APNS)
         // could delay it if device is sleeping
         alert.setNotificationPriority(Alert.Priority.normal);
+
+        alert.setCustomUrl(url);
 
         return apiClient.publish(alert);
     }
@@ -184,35 +185,50 @@ public class Demo {
         return apiClient.getTag(id);
     }
 
-	private static Properties loadProperties() throws IOException {
-		InputStream fileUrl = Demo.class.getResourceAsStream("/publisher.properties");
-		Properties properties = new Properties();
-		properties.load(fileUrl);
+    private static Properties loadProperties() throws IOException {
+        InputStream fileUrl = Demo.class.getResourceAsStream("/publisher.properties");
+        Properties properties = new Properties();
+        properties.load(fileUrl);
         return properties;
-	}
-	
-	private static StringBuffer getText(String[] args) throws IOException {
-		StringBuffer text = new StringBuffer();
+    }
+
+    private static StringBuffer getText(String[] args) throws IOException {
+        StringBuffer text = new StringBuffer();
         String defaultText = "Hello World!";
-		if (args.length > 0) {
-			if (args[0].toLowerCase().trim().equals("--file")) {
-				String filename = args[1].trim();
-				FileReader fileReader = new FileReader(filename);
-				BufferedReader bufferedReader = new BufferedReader(fileReader);
-				String line;
-				while ((line = bufferedReader.readLine()) != null) {
-					text.append(line);
-					text.append("\n");
-				}
-				fileReader.close();
-			} else if (args[0].toLowerCase().trim().equals("--text")) {
-				text.append(args[1].trim());
-			} else {
-				text.append(defaultText);
-			}
-		} else {
-			text.append(defaultText);
-		}
-		return text;
-	}
+        if (args.length > 0) {
+            if (args[0].toLowerCase().trim().equals("--file")) {
+                String filename = args[1].trim();
+                FileReader fileReader = new FileReader(filename);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    text.append(line);
+                    text.append("\n");
+                }
+                fileReader.close();
+            } else if (args[0].toLowerCase().trim().equals("--text")) {
+                text.append(args[1].trim());
+            } else {
+                text.append(defaultText);
+            }
+        } else {
+            text.append(defaultText);
+        }
+        return text;
+    }
+
+    private static StringBuffer getUrl(String[] args) throws IOException {
+        StringBuffer text = new StringBuffer();
+        String defaultText = "http://autoportal.com/upcomingcars/model/au";
+        if (args.length > 0) {
+            if (args[2].toLowerCase().trim().equals("--url")) {
+                text.append(args[3].trim());
+            } else {
+                text.append(defaultText);
+            }
+        } else {
+            text.append(defaultText);
+        }
+        return text;
+    }
 }
--- java-publisher-client/src/main/java/io/boxcar/publisher/model/Alert.java
+++ java-publisher-client/src/main/java/io/boxcar/publisher/model/Alert.java
@@ -2,6 +2,7 @@ package io.boxcar.publisher.model;
 
 import io.boxcar.publisher.client.util.TimeUtils;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,26 +10,26 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents the "content" to be published
- * @author jpcarlino
  *
+ * @author jpcarlino
  */
 public class Alert<T> {
 
     public static final String ALL_PUSH_TAG = "@all";
 
-	public static class I18nAlert {
+    public static class I18nAlert {
         @SerializedName("loc-key")
         public String key;
         @SerializedName("loc-args")
         public String[] args;
     }
 
-	public class Aps<T> {
-		String badge;
-		String sound;
-		String category;
-		T alert;
-	}
+    public class Aps<T> {
+        String badge;
+        String sound;
+        String category;
+        T alert;
+    }
 
     public enum Behavior {
         last_app_open, last_app_registered
@@ -58,6 +59,7 @@ public class Alert<T> {
             this.interval = interval;
             this.exclude = false;
         }
+
         Behavior behavior;
         boolean exclude;
         Interval interval;
@@ -72,170 +74,175 @@ public class Alert<T> {
 
     }
 
-	Aps<T> aps;
+    Aps<T> aps;
 
     @SerializedName("behavior_segments")
     List<BehaviorSegment> segments;
-	List<String> tags;
-	List<String> target_os;
-	List<Integer> client_ids;
-	String id;
-	long expires;
-	Integer expires_after;
-	List<String> device_tokens;
-        Priority notification_priority;
-	
-	@SerializedName("@img")
-	String img;
+    List<String> tags;
+    List<String> target_os;
+    List<Integer> client_ids;
+    String id;
+    long expires;
+    Integer expires_after;
+    List<String> device_tokens;
+    Priority notification_priority;
 
-	public Alert(T content) {
-		this.aps = new Aps<T>();
-		this.aps.badge = "auto";
-		this.aps.sound = "beep.wav";
-		this.aps.category = null;
-		this.aps.alert = content;
-		this.tags = new ArrayList<String>();
-		this.id = null;
-		this.target_os = null;
-		this.client_ids = null;
-		this.device_tokens = null;
-		this.expires_after = null; // do not set TTL by default
-		this.img = null;
-		this.segments = null;
-                this.notification_priority = null;
-		setAPICallTimeToLive(30000);
-	}
-	
-	public Alert(T content, String id) {
-		this.aps = new Aps<T>();
-		this.aps.badge = "auto";
-		this.aps.sound = "beep.wav";
-		this.aps.category = null;
-		this.aps.alert = content;
-		this.tags = new ArrayList<String>();
-		this.id = id;
-		this.target_os = null;
-		this.client_ids = null;
-		this.device_tokens = null;
-		this.expires_after = null; // do not set TTL by default
-		this.img = null;
-                this.notification_priority = null;
+    @SerializedName("@img")
+    String img;
+
+    @SerializedName("custom_url")
+    String customUrl;
+
+    public Alert(T content) {
+        this.aps = new Aps<T>();
+        this.aps.badge = "auto";
+        this.aps.sound = "beep.wav";
+        this.aps.category = null;
+        this.aps.alert = content;
+        this.tags = new ArrayList<String>();
+        this.id = null;
+        this.target_os = null;
+        this.client_ids = null;
+        this.device_tokens = null;
+        this.expires_after = null; // do not set TTL by default
+        this.img = null;
         this.segments = null;
-		setAPICallTimeToLive(30000);
-	}
+        this.notification_priority = null;
+        setAPICallTimeToLive(30000);
+    }
 
-	public T getAlert() {
-		return aps.alert;
-	}
+    public Alert(T content, String id) {
+        this.aps = new Aps<T>();
+        this.aps.badge = "auto";
+        this.aps.sound = "beep.wav";
+        this.aps.category = null;
+        this.aps.alert = content;
+        this.tags = new ArrayList<String>();
+        this.id = id;
+        this.target_os = null;
+        this.client_ids = null;
+        this.device_tokens = null;
+        this.expires_after = null; // do not set TTL by default
+        this.img = null;
+        this.notification_priority = null;
+        this.segments = null;
+        setAPICallTimeToLive(30000);
+    }
 
-	public void setAlert(T content) {
-		aps.alert = content;
-	}
-	
-	public String getSound() {
-		return aps.sound;
-	}
+    public T getAlert() {
+        return aps.alert;
+    }
 
-	public void setSound(String sound) {
-		aps.sound = sound;
-	}
+    public void setAlert(T content) {
+        aps.alert = content;
+    }
 
-	public String getCategory() {
-		return aps.category;
-	}
+    public String getSound() {
+        return aps.sound;
+    }
 
-	public void setCategory(String category) {
-		aps.category = category;
-	}
-	
-	public String getBadge() {
-		return aps.badge;
-	}
+    public void setSound(String sound) {
+        aps.sound = sound;
+    }
 
-	public void setBadge(String badge) {
-		aps.badge = badge;
-	}
-	
-	public List<String> getTags() {
-		return tags;
-	}
+    public String getCategory() {
+        return aps.category;
+    }
 
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
+    public void setCategory(String category) {
+        aps.category = category;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getBadge() {
+        return aps.badge;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setBadge(String badge) {
+        aps.badge = badge;
+    }
 
-	/**
-	 * Sets the Time To Live (TTL) in seconds for this push.
-	 * This value is used by the gateway provider (Google GCM, Apple APNS,
-	 * Amazon ADM, etc) to know how much time to keep the push if device
-	 * is offline.
-	 * Ignored if value <= 0
-	 * @param seconds TTL in seconds. It won't be considered for delivery
-	 * if <= 0
-	 */
-	public void setTTL(int seconds) {
-		this.expires_after = seconds;
-	}
-	
-	public int getTTL() {
-		if (expires_after == null) {
-			return -1;
-		}
-		return this.expires_after;
-	}
-	
-	public List<String> getTargetOS() {
-		return target_os;
-	}
+    public List<String> getTags() {
+        return tags;
+    }
 
-	public void setTargetOS(List<String> targetOS) {
-		this.target_os = targetOS;
-	}
-	
-	public void setTargetOS(String targetOS) {
-		this.target_os = new ArrayList<String>();
-		this.target_os.add(targetOS);
-	}
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 
-	public List<Integer> getClientIds() {
-		return client_ids;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setClientIds(List<Integer> clientIds) {
-		this.client_ids = clientIds;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public List<String> getDeviceTokens() {
-		return device_tokens;
-	}
+    /**
+     * Sets the Time To Live (TTL) in seconds for this push.
+     * This value is used by the gateway provider (Google GCM, Apple APNS,
+     * Amazon ADM, etc) to know how much time to keep the push if device
+     * is offline.
+     * Ignored if value <= 0
+     *
+     * @param seconds TTL in seconds. It won't be considered for delivery
+     *                if <= 0
+     */
+    public void setTTL(int seconds) {
+        this.expires_after = seconds;
+    }
 
-	public void setDeviceTokens(List<String> deviceTokens) {
-		this.device_tokens = deviceTokens;
-	}
-
-	public String getBigImageURL() {
-		return img;
-	}
-
-	public void setBigImageURL(String url) {
-		this.img = url;
-	}
-
-        public Priority getNotificationPriority() {
-		return notification_priority;
+    public int getTTL() {
+        if (expires_after == null) {
+            return -1;
         }
+        return this.expires_after;
+    }
 
-        public void setNotificationPriority(Priority notificationPriority) {
-		this.notification_priority = notificationPriority;
-        }
+    public List<String> getTargetOS() {
+        return target_os;
+    }
+
+    public void setTargetOS(List<String> targetOS) {
+        this.target_os = targetOS;
+    }
+
+    public void setTargetOS(String targetOS) {
+        this.target_os = new ArrayList<String>();
+        this.target_os.add(targetOS);
+    }
+
+    public List<Integer> getClientIds() {
+        return client_ids;
+    }
+
+    public void setClientIds(List<Integer> clientIds) {
+        this.client_ids = clientIds;
+    }
+
+    public List<String> getDeviceTokens() {
+        return device_tokens;
+    }
+
+    public void setDeviceTokens(List<String> deviceTokens) {
+        this.device_tokens = deviceTokens;
+    }
+
+    public String getBigImageURL() {
+        return img;
+    }
+
+    public void setBigImageURL(String url) {
+        this.img = url;
+    }
+
+    public Priority getNotificationPriority() {
+        return notification_priority;
+    }
+
+    public void setNotificationPriority(Priority notificationPriority) {
+        this.notification_priority = notificationPriority;
+    }
+
     public List<BehaviorSegment> getBehaviorSegments() {
         return segments;
     }
@@ -244,8 +251,15 @@ public class Alert<T> {
         this.segments = segments;
     }
 
-	protected void setAPICallTimeToLive(long millis) {
-		this.expires = TimeUtils.getExpiryDate(millis).getTime() / 1000;
-	}
+    protected void setAPICallTimeToLive(long millis) {
+        this.expires = TimeUtils.getExpiryDate(millis).getTime() / 1000;
+    }
 
+    public String getCustomUrl() {
+        return customUrl;
+    }
+
+    public void setCustomUrl(String customUrl) {
+        this.customUrl = customUrl;
+    }
 }
